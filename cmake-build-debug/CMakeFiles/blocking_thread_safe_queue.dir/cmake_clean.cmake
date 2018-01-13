37ce485bc058e8bf156e2563308b812ed7079d1f
Leo Chen <chensili.uestc@gmail.com>
2018-01-13 10:20:35 -0500

    init commit
    repository created

---
+++ cmake-build-debug/CMakeFiles/blocking_thread_safe_queue.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/blocking_thread_safe_queue.dir/main.cpp.o"
+  "blocking_thread_safe_queue.pdb"
+  "blocking_thread_safe_queue"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/blocking_thread_safe_queue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
